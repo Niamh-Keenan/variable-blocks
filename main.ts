3b195eeb0633e63ef297dd6b014db86195e8fdeb
Niamh-Keenan <113034875+Niamh-Keenan@users.noreply.github.com>
2022-09-27 09:21:50 -0400

    added a comment

--- main.ts
+++ main.ts
@@ -7,5 +7,9 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     Counter += -1
 })
+// Created_by: Niamh_Keenan
+// Created_on: September_27th_2022
+// 
+// This_program_increases_or_decreases_the_counter_based_on_what_button_you_press
 let Counter = 0
 Counter = 0
